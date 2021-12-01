024fe51ca2e5f5cf4f62098085cca4c471fb9c0c
Manzanit0 <unico.javier@gmail.com>
2021-12-01 19:47:38 +0100

    Wrap up elixir day 1

--- 2021/day_1.exs
+++ 2021/day_1.exs
@@ -1,19 +1,54 @@
 #! /usr/bin/env elixir
 
 # Much iteration going on... but can't be bothered to fit it into the same reduce.
+
+count_increasing = fn list ->
+  Enum.reduce(list, {nil, 0}, fn
+    x, {nil, 0} ->
+      {x, 0}
+
+    x, {prev, sum} when x > prev ->
+      {x, sum + 1}
+
+    x, {_prev, sum} ->
+      {x, sum}
+  end)
+end
+
+parse_input = fn content ->
+  content
+  |> String.split("\n")
+  |> Enum.filter(fn x -> x != "" end)
+  |> Enum.map(&String.to_integer/1)
+end
+
+### PART 1
+IO.write("Day 1 part 1: ")
+
 File.read!("./input_1.txt")
-|> String.split("\n")
-|> Enum.filter(fn x -> x != "" end)
-|> Enum.map(&String.to_integer/1)
-|> Enum.reduce({nil, 0}, fn
-  x, {nil, 0} ->
-    {x, 0}
+|> then(parse_input)
+|> then(count_increasing)
+|> Kernel.elem(1)
+|> IO.inspect()
 
-  x, {prev, sum} when x > prev ->
-    {x, sum + 1}
+### PART 2
+calculate_windows = fn [f, s, t | list] ->
+  initial_window = [f, s, t]
 
-  x, {_prev, sum} ->
-    {x, sum}
-end)
+  # We map the window sum, but accumulate the actual elements to slide in the
+  # following interation.
+  {window_sums, _} =
+    Enum.map_reduce(list, initial_window, fn x, [_, s, t] -> {s + t + x, [s, t, x]} end)
+
+  initial_window_sum = Enum.sum(initial_window)
+  [initial_window_sum | window_sums]
+end
+
+IO.write("Day 1 part 2: ")
+
+File.read!("./input_2.txt")
+|> then(parse_input)
+|> then(calculate_windows)
+|> then(count_increasing)
 |> Kernel.elem(1)
 |> IO.inspect()
